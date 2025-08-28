7de1af897445aa3ddb9fb2793c3661067e37ca86
KhaiNguyenHoang <khaiproject1234@gmail.com>
2025-08-28 17:47:20 +0700

    Update lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,30 +1,26 @@
 ---@type LazySpec
 return {
-  "glepnir/lspsaga.nvim",
+  -- "glepnir/lspsaga.nvim",
+  "leeguooooo/lspsaga.nvim",
+  branch = "fix-client-method-deprecations",
   event = "LspAttach",
   cmd = "Lspsaga",
   enabled = true,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     {
-      "AstroNvim/astrocore",
-      opts = function(_, opts)
-        local maps = opts.mappings
-        maps.n["]d"] = { "<Cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next diagnostic" }
-        maps.n["[d"] = { "<Cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Previous diagnostic" }
-        -- disable default aerial symbols document
-        maps.n["<Leader>lS"] = nil
-        maps.n["<Leader>ls"] = nil
-      end,
-    },
-    {
       "AstroNvim/astrolsp",
       opts = function(_, opts)
         local maps = opts.mappings
         -- disabl default mapping
-        maps.n["<Leader>lA"] = nil
-        maps.n["<Leader>lG"] = nil
+        if not opts.mappings then opts.mappings = require("astrocore").empty_map_table() end
+        -- maps.n["<Leader>lA"] = nil
+        -- maps.n["<Leader>lG"] = nil
+        -- disable default aerial symbols document
+        -- maps.n["<Leader>lS"] = nil
 
+        maps.n["]d"] = { "<Cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next diagnostic" }
+        maps.n["[d"] = { "<Cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Previous diagnostic" }
         -- hover
         -- maps.n["K"] = { "<Cmd>Lspsaga hover_doc<CR>", desc = "Hover symbol details", cond = "textDocument/hover" }
 
@@ -35,38 +31,53 @@ return {
           { "<Cmd>Lspsaga outgoing_calls<CR>", desc = "Outgoing calls", cond = "callHierarchy/outgoingCalls" }
 
         -- code action
-        -- maps.n["<Leader>la"] =
-        --   { "<Cmd>Lspsaga code_action<CR>", desc = "Code action", cond = "textDocument/codeAction" }
-        -- maps.x["<Leader>la"] =
-        --   { ":<C-U>Lspsaga code_action<CR>", desc = "Code action", cond = "textDocument/codeAction" }
+        -- NOTE: Bug use aznhe21/actions-preview.nvim instead
+        maps.n["<Leader>la"] =
+          { "<Cmd>Lspsaga code_action<CR>", desc = "Code action", cond = "textDocument/codeAction" }
+        maps.x["<Leader>la"] =
+          { ":<C-U>Lspsaga code_action<CR>", desc = "Code action", cond = "textDocument/codeAction" }
 
         -- diagnostic
         maps.n["<Leader>ld"] =
           { "<Cmd>Lspsaga show_line_diagnostics ++unfocus<CR>", desc = "Diagnostics in line", cond = "" }
 
         -- definition
-        maps.n["<Leader>lp"] =
-          { "<Cmd>Lspsaga peek_definition<CR>", desc = "Peek definition", cond = "textDocument/definition" }
-        -- maps.n["gd"] =
+        -- maps.n["<Leader>lp"] =
         --   { "<Cmd>Lspsaga peek_definition<CR>", desc = "Peek definition", cond = "textDocument/definition" }
+        maps.n["gd"] =
+          { "<Cmd>Lspsaga peek_definition<CR>", desc = "Peek definition", cond = "textDocument/definition" }
 
         -- outline
-        maps.n["<Leader>ls"] =
-          { "<Cmd>Lspsaga outline<CR>", desc = "Symbols outline", cond = "textDocument/documentSymbol" }
+        -- use aerial
+        -- maps.n["<Leader>ls"] =
+        --   { "<Cmd>Lspsaga outline<CR>", desc = "Symbols outline", cond = "textDocument/documentSymbol" }
 
         -- references
-        maps.n["<Leader>lR"] = {
-          "<Cmd>Lspsaga finder<CR>",
-          desc = "Search references",
-          cond = function(client)
-            return client.supports_method "textDocument/references"
-              or client.supports_method "textDocument/implementation"
-          end,
-        }
+        -- NOTE: BUG
+        -- maps.n["<Leader>lR"] = {
+        --   "<Cmd>Lspsaga finder<CR>",
+        --   desc = "Search references",
+        --   cond = function(client)
+        --     return client.supports_method "textDocument/references"
+        --       or client.supports_method "textDocument/implementation"
+        --   end,
+        -- }
 
         -- rename
         maps.n["<Leader>lr"] =
           { "<Cmd>Lspsaga rename<CR>", desc = "Rename current symbol", cond = "textDocument/rename" }
+        -- Close rename popup if press Esc in normal mode
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = "sagarename",
+          callback = function(event)
+            vim.keymap.set(
+              "n",
+              "<Esc>",
+              "<cmd>confirm q<cr>",
+              { buffer = event.buf, silent = true, cond = "textDocument/rename" }
+            )
+          end,
+        })
       end,
     },
   },
@@ -79,16 +90,18 @@ return {
       max_height = 0.5,
       keys = {
         shuttle = "p",
-        edit = { "o", "<CR>" },
-        vsplit = "s",
-        split = "i",
-        tabe = "",
         tabnew = "",
         quit = "q",
+        edit = "<C-c>o",
+        vsplit = "<C-c>v",
+        split = "<C-c>i",
+        tabe = "<C-c>t",
         close = "<ESC>",
       },
     }
     opts.definition = {
+      width = 0.8,
+      height = 0.8,
       keys = {
         edit = "<C-c>o",
         vsplit = "<C-c>v",
@@ -115,16 +128,11 @@ return {
       virtual_text = true,
     }
     opts.diagnostic = {
-      on_insert = false,
-      on_insert_follow = false,
-      insert_winblend = 0,
       show_code_action = true,
       show_source = true,
       jump_num_shortcut = true,
       --1 is max
-      max_width = 0.7,
-      custom_fix = nil,
-      custom_msg = nil,
+      max_width = 0.8,
       extend_relatedInformation = true,
       -- text_hl_follow = false,
       -- border_follow = false,
@@ -132,11 +140,13 @@ return {
         exec_action = "o",
         quit = "q",
         go_action = "g",
+        toggle_or_jump = "<CR>",
+        quit_in_show = { "q", "<ESC>" },
       },
     }
     opts.rename = {
       keys = {
-        quit = "<ESC>",
+        quit = "<C-k>",
         exec = "<CR>",
         select = "x",
         confirm = "<CR>",
